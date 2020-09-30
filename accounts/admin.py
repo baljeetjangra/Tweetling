ce7e8c6d4752814e0a62442d1d028b4a4e97bda2
Baljeet Jangra <baljeetjangra45@gmail.com>
2020-09-30 15:12:34 +0530

    Custom User Model Created For Signup and Login With Email

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import User
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ('email','is_staff','is_active')
+    list_filter = ('email','is_staff','is_active')
+    fieldsets = (
+        (None, {'fields': ('first_name','last_name','email', 'password',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('first_name','last_name','email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering =('email',)
+
+admin.site.register(User, CustomUserAdmin)
--- tweetling/settings.py
+++ tweetling/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tweet',
+    'accounts',
+    # 'tweet',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
