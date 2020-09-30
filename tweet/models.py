8ddd95abb2878ab43291ce124d51a07383fb6dda
Baljeet Jangra <baljeetjangra45@gmail.com>
2020-09-30 13:07:06 +0530

    accounts app added

--- tweet/models.py
+++ tweet/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+# class Tweet(models.Model):
+#     tweet = models.CharField(max_length=250)
+#     image = models.ImageField(upload_to=None, null=True, blank=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
